dbab8c14fa1d19fdaf7000aec44be10871f3f92c
MetrySean <sean.mtr@eduge.ch>
2017-02-08 15:26:00 +0100

    simple Update de l'interface

--- ReplicationExcel/ConvertListToFile.cs
+++ ReplicationExcel/ConvertListToFile.cs
@@ -1,18 +1,6 @@
-﻿/* *
- * *-*-*-*-*-*-*-*-*-*-*-*-*-*
- *        Version 1.0
- * -*-*-*-*-*-*-*-*-*-*-*-*-*-*
- * Auteurs : Bacaicoa Thomas, Geier Phillip, Argelli Angelo, Ehlers Thomas, Schupbach Loïc
+﻿/* Auteurs : Bacaicoa Thomas, Geier Phillip, Argelli Angelo, Ehlers Thomas, Schupbach Loïc
  * Version 1.0
  * Date : 12 mars 2015
- * Classe : IFA-P3B
- * 
- * *-*-*-*-*-*-*-*-*-*-*-*-*-*
- *        Version 2.0
- * -*-*-*-*-*-*-*-*-*-*-*-*-*-*
- *  * Auteurs : Gabor Tagliabue, Jessica Sulzbach, Dilan Marques, Sean Metry
- * Date de debut : 01/02/2016
- * Date de fin : 
  * Classe : IFA-P3B
  */
 
